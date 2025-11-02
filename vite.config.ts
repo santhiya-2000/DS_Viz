de637c8bb65d45964fd4dbea5f9ed52371e02948
Santhiya <santhiya@Sandys-Air-2.lan>
2025-11-01 21:48:03 -0400

    changes 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/DS_Viz/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/DS_Viz/' : '/',
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    open: true,
+  },
+  preview: {
+    port: 3000,
+    open: true,
+  },
+}))
